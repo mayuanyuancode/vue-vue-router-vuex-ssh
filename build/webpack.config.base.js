569a818c461a1d1e70b24d6fd7f9a99c4413e72f
MYYFirstGit <myy120@outlook.com>
2019-02-27 18:11:40 +0800

    初始启动服务器，不同端口号加载

---
+++ build/webpack.config.base.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const createVueLoaderOptions = require('./vue-loader.config')
+
+const isDev = process.env.NODE_ENV === 'development'
+
+const config = {
+  target: 'web',
+  entry: path.join(__dirname, '../client/client-entry.js'),
+  output: {
+    filename: 'bundle.[hash:8].js',
+    path: path.join(__dirname, '../public'),
+    // 倘若想通过端口号 3333 访问 8000的静态资源，只能写死publicPath，默认的dev server，package.json dev 指向的"npm run dev:client\" \"npm run dev:server\"
+    publicPath: 'http://127.0.0.1:8000/public/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(vue|js|jsx)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+        enforce: 'pre'
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: createVueLoaderOptions(isDev)
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              name: 'resources/[path][name].[hash:8].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
+
+module.exports = config
---
+++ build/webpack.config.practice.js
@@ -0,0 +1,67 @@
+const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.config.base')
+
+const defaultPluins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: '"development"'
+    }
+  }),
+  new HTMLPlugin({
+    template: path.join(__dirname, 'template.html')
+  })
+]
+// npm run practice 默认启动该devServer
+const devServer = {
+  port: 8080,
+  host: '0.0.0.0',
+  overlay: {
+    errors: true
+  },
+  hot: true
+}
+
+let config
+
+// 通过webpack merge加载一个对象，对象中无output所以执行 webpack.config.base.js 的output
+config = merge(baseConfig, {
+  output: {
+    publicPath: '/public/'
+  },
+  entry: path.join(__dirname, '../practice/index.js'),
+  devtool: '#cheap-module-eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.styl/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          'stylus-loader'
+        ]
+      }
+    ]
+  },
+  devServer,
+  // import Vue from 'vue'
+  resolve: {
+    alias: {
+      'vue': path.join(__dirname, '../node_modules/vue/dist/vue.esm.js')
+    }
+  },
+  plugins: defaultPluins.concat([
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ])
+})
+
+module.exports = config
